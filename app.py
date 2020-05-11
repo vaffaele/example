c53f9a97eef869dcd7114dd15a03c587e9ad3bb3
vaffaele <raf.sollecito@gmail.com>
2020-05-11 12:43:17 +0200

    last

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ from functools import wraps
 import json
 from os import environ as env
 from werkzeug.exceptions import HTTPException
-from dotenv import load_dotenv, find_dotenv
 from flask import session
 from flask import url_for
 from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
 from flask_cors import cross_origin
-
+from dotenv import load_dotenv, find_dotenv
 
 
 
